c56d12a4e387c199b246e93386775e88f3e9da23
ykm <1621970846@qq.com>
2019-04-08 16:44:16 +0800

    123

--- jQuery/header-footer.js
+++ jQuery/header-footer.js
@@ -12,7 +12,12 @@ $(function () {
         }else if($(window).width() < 768 && $(".dropdown-menu").css("display") == "block"){
             $(".seek").css("display","block");
         }
-    })
+        $(document).on("click",function(){
+            $(".seek").css("display","block");
+        });
+
+    });
+
 
     
     // 滚动条
